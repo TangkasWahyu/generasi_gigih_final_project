1aa030dc2a6a284fb0dc5f53d9eda9df5f272ce8
Yudo Kartiko <yudoworks@gmail.com>
2021-08-09 15:27:58 +0700

    Revert "Add PostController.save and Add API to save post"
    This reverts commit 35cc9bd9be3b7875cae2d6c39b465111f0cc09af.

--- controllers/post_controller.rb
+++
@@ -1,8 +0,0 @@
-require_relative '../models/post'
-
-class PostController
-    def self.save(params)
-        post = Post.new(params)
-        post.save(params)
-    end
-end
--- main.rb
+++ main.rb
@@ -4,7 +4,3 @@ require_relative 'controllers/user_controller'
 post '/users/save' do
     UserController.save(params)
 end
-
-post '/users/:id/posts/create' do
-    UserController.save(params)
-end
