f4d315a1e56c86734d1d97c1f68ad13951c6e1e6
Yudo Kartiko <yudoworks@gmail.com>
2021-08-10 11:12:32 +0700

    Add User.get_by_id and add id when initialize user
    Add test and implementation. User.get_by_id when given id is 1 it should call get_by_id_query with id equal 1 and get user with id 1.
    
    Add id when initialize user with the attr_accessor

--- models/user.rb
+++ models/user.rb
@@ -1,12 +1,13 @@
 require_relative '../db/mysql_connector'
 
 class User
-    attr_accessor :username, :email, :bio_description
+    attr_accessor :username, :email, :bio_description, :id
 
     def initialize(attribute)
         @username = attribute["username"]
         @email = attribute["email"]
         @bio_description = attribute["bio_description"]
+        @id = attribute["id"]
     end
 
     def save
@@ -17,6 +18,16 @@ class User
     end
 
     def self.get_by_id(id)
+        client = create_db_client
+        users = Array.new
+    
+        rawData = client.query("select * from users where id = #{id}")
+    
+        rawData.each do |data|
+            user = User.new(data);
+            users.push(user)
+        end
 
+        users.pop
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -39,4 +39,29 @@ describe User do
         end
     end
     
+    describe ".get_by_id" do
+        context "given id is 1" do
+            it "should call get_by_id_query with id equal 1 and get user with id 1" do
+                mock_client = double
+                id = "1"
+                get_by_id_query = "select * from users where id = #{id}"
+                valid_attribute = {
+                    "id" => "1",
+                    "username" => "mark",
+                    "email" => "mark@mail.com",
+                    "bio_description" => "20 years old and grow"
+                }
+                rawData = [valid_attribute]                
+
+                allow(Mysql2::Client).to receive(:new).and_return(mock_client)
+                expect(mock_client).to receive(:query).with(get_by_id_query).and_return(rawData)
+                expect(get_by_id_query).to include(id) 
+
+                user = User.get_by_id(id)
+
+                expect(user.id).to eq(id) 
+            end
+        end
+    end
+    
 end
