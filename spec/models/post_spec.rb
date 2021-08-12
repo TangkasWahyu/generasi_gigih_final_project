6c87aaab3f1b5d6d7dd17d29a2ca989ec786dd66
Yudo Kartiko <yudoworks@gmail.com>
2021-08-12 10:09:52 +0700

    Move text attribute and method to post
    Signed-off-by: Yudo Kartiko <yudoworks@gmail.com>

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,18 +1,17 @@
 require_relative '../test_helper'
-require_relative '../../models/text'
 require_relative '../../models/user'
 require_relative '../../models/post'
 
-describe Text do
-    let(:text_valid_attribute) {{
+describe Post do
+    let(:post_valid_attribute) {{
         "text" => "Hello world"
     }}
-    let(:text_valid_attribute_with_id) {{
+    let(:post_valid_attribute_with_id) {{
         "id" => "1",
         "text" => "Hello world"
     }}
-    let(:text) { Text.new text_valid_attribute }
-    let(:text_with_id) { Text.new text_valid_attribute_with_id }
+    let(:post) { Post.new post_valid_attribute }
+    let(:post_with_id) { Post.new post_valid_attribute_with_id }
     let(:mock_client) {double}
 
     before(:each) do
@@ -22,7 +21,7 @@ describe Text do
     describe ".initialize" do
         context "given valid attribute" do
             it "should create object that equal with valid_attribute" do
-                expect(text.text).to eq(text_valid_attribute["text"])
+                expect(post.text).to eq(post_valid_attribute["text"])
             end
         end
     end
@@ -32,13 +31,13 @@ describe Text do
             it "should call hashtags and insert_post_hashtags_query" do
                 hashtags = ["monday"]
                 hashtag_ids = ["1"]
-                insert_post_hashtags_query = "insert into postHashtags (post_id, hashtag_id) values (#{text_with_id.id}, #{hashtag_ids[0]})"
+                insert_post_hashtags_query = "insert into postHashtags (post_id, hashtag_id) values (#{post_with_id.id}, #{hashtag_ids[0]})"
                 
-                allow(text_with_id).to receive(:get_hashtags).and_return(hashtags)
+                allow(post_with_id).to receive(:get_hashtags).and_return(hashtags)
                 expect(Hashtag).to receive(:save_hashtags).with(hashtags).and_return(hashtag_ids)
                 expect(mock_client).to receive(:query).with(insert_post_hashtags_query)
     
-                text_with_id.save_hashtags
+                post_with_id.save_hashtags
             end
         end
     end
@@ -50,7 +49,7 @@ describe Text do
                 valid_post_attribute_with_1_hashtag = {
                     "text" => "Hello world #monday"
                 }
-                post = Text.new(valid_post_attribute_with_1_hashtag)
+                post = Post.new(valid_post_attribute_with_1_hashtag)
 
                 actual = post.get_hashtags
 
@@ -64,7 +63,7 @@ describe Text do
                 valid_post_attribute_without_hashtag = {
                     "text" => "Hello world"
                 }
-                post = Text.new(valid_post_attribute_without_hashtag)
+                post = Post.new(valid_post_attribute_without_hashtag)
 
                 actual = post.get_hashtags
 
@@ -78,7 +77,7 @@ describe Text do
                 valid_post_attribute_with_1_uppercase_hashtag = {
                     "text" => "Hello world #Monday"
                 }
-                post = Text.new(valid_post_attribute_with_1_uppercase_hashtag)
+                post = Post.new(valid_post_attribute_with_1_uppercase_hashtag)
 
                 actual = post.get_hashtags
 
@@ -92,7 +91,7 @@ describe Text do
                 valid_post_attribute_with_2_hashtag = {
                     "text" => "Hello world #monday #tuesday"
                 }
-                post = Text.new(valid_post_attribute_with_2_hashtag)
+                post = Post.new(valid_post_attribute_with_2_hashtag)
 
                 actual = post.get_hashtags
 
@@ -106,7 +105,7 @@ describe Text do
                 valid_post_attribute_with_2_same_hashtag = {
                     "text" => "Hello world #monday #monday"
                 }
-                post = Text.new(valid_post_attribute_with_2_same_hashtag)
+                post = Post.new(valid_post_attribute_with_2_same_hashtag)
 
                 actual = post.get_hashtags
 
@@ -121,7 +120,7 @@ describe Text do
                 post_attribute = {
                     "text" => "Hello world #monday"
                 }
-                post = Text.new(post_attribute)
+                post = Post.new(post_attribute)
 
                 actual = post.is_characters_maximum_limit?
 
@@ -135,7 +134,7 @@ describe Text do
                 post_attribute = {
                     "text" => text
                 }
-                post = Text.new(post_attribute)
+                post = Post.new(post_attribute)
 
                 actual = post.is_characters_maximum_limit?
 
@@ -149,7 +148,7 @@ describe Text do
                 post_attribute = {
                     "text" => text
                 }
-                post = Text.new(post_attribute)
+                post = Post.new(post_attribute)
 
                 actual = post.is_characters_maximum_limit?
 
@@ -189,9 +188,9 @@ describe Text do
             it "should post.user equal user_mock" do
                 user_mock = double
 
-                text.add_user(user_mock)
+                post.add_user(user_mock)
 
-                expect(text.user).to eq(user_mock)
+                expect(post.user).to eq(user_mock)
             end
         end
     end
