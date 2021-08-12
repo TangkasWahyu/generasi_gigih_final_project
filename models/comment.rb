f6009cfa04fc9fd4dec14cefbe686fa58ca74293
Yudo Kartiko <yudoworks@gmail.com>
2021-08-12 11:12:46 +0700

    Add Comment#send test and implementation
    Comment#send it should call insert_comment_query
    
    Signed-off-by: Yudo Kartiko <yudoworks@gmail.com>

--- models/comment.rb
+++ models/comment.rb
@@ -1,4 +1,26 @@
 require_relative 'post'
+require_relative 'hashtag'
 
 class Comment < Post
+    attr_reader :post
+
+    def initialize(attribute)
+        super(attribute)
+        @post = attribute["post"]
+    end
+    
+    def send
+        client = create_db_client
+        return if self.is_characters_maximum_limit?
+
+        client = create_db_client
+        insert_comment_query = "insert into comments (user_id, post_id, text) values ('#{@user.id}', '#{@post.id}', '#{@text}')"
+
+        client.query(insert_comment_query)
+        comment_id = client.last_id
+
+        @id = comment_id
+
+        self.save_hashtags
+    end
 end
