52ba47f3891a76546b6f6f585b05a29766cfaf37
Yudo Kartiko <yudoworks@gmail.com>
2021-08-13 15:39:13 +0700

    Use let ini attachment_spec

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -2,16 +2,15 @@ require_relative '../test_helper'
 require_relative '../../models/attachment'
 
 describe Attachment do
+    let(:file_mock) { double }
+    let(:attachment_attribute) {{
+        "filename" => "filename", 
+        "tempfile" => file_mock
+    }}
+    
     describe "#initialize" do
         context "given attachment_attribute" do
             it "should expected(filename, file) equal to attachment_attribute(filename, tempfile)" do
-                attachment_attribute = {
-                    "filename" => "filename", 
-                    "type" => "filetype", 
-                    "name" => "name", 
-                    "tempfile" => "tempfile"
-                }
-                        
                 expected = Attachment.new(attachment_attribute)
         
                 expect(expected.filename).to eq(attachment_attribute["filename"])
@@ -23,12 +22,7 @@ describe Attachment do
     describe "#save" do
         it "should call save_location" do
             f_mock = double
-            file_mock = double
             file_read = double
-            attachment_attribute = {
-                "filename" => "filename", 
-                "tempfile" => file_mock
-            }
             attachment = Attachment.new(attachment_attribute)
             save_location = "./public/#{attachment.filename}"
 
