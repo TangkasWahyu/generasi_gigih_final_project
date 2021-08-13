1553bd033818c003427525fd255562c56bdd4afd
Yudo Kartiko <yudoworks@gmail.com>
2021-08-13 23:55:18 +0700

    Add attachment#is_valid? test and implementation
    attachment#is_valid? when attachment type is video/mp4 it should be true.
    
    attachment#is_valid? when attachment type is image/png it should be true.
    
    attachment#is_valid? when attachment type is image/gif it should be true
    
    attachment#is_valid? when attachment type is image/jpeg it should be true.
    
    attachment#is_valid? when attachment type is text/plain it should be true.
    
    attachment#is_valid? when attachment type is text/csv it should be true.
    
    attachment#is_valid? when attachment type is application/x-tar it should be false.
    
    Signed-off-by: Yudo Kartiko <yudoworks@gmail.com>

--- models/attachment.rb
+++ models/attachment.rb
@@ -3,15 +3,24 @@ class Attachment
 
     def initialize(attribute)
         @filename = attribute["filename"]
+        @type = attribute["type"]
         @file = attribute["tempfile"]
     end
 
     def save
+        return unless is_allowed?
+
         save_location = "./public/#{@filename}"
         file_read = @file.read
         
         File.open(save_location, 'wb') do |f|
             f.write(file_read)
         end
+    end
+
+    def is_allowed?
+        return true if ["video/mp4", "image/png", "image/gif", "image/jpeg"].include?(@type) || (@type.include?("text"))
+
+        false
     end
 end
