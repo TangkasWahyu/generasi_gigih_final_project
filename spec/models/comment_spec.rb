4d25675e6090fb694ecb9dffdccb8e91d4ce497e
Yudo Kartiko <yudoworks@gmail.com>
2021-08-17 16:30:42 +0700

    Fix invalid test comment#save have user with id and post it should call mock_query and insert_post_ref_query.
    Signed-off-by: Yudo Kartiko <yudoworks@gmail.com>

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,8 +6,9 @@ require_relative '../../models/post'
 describe Comment do
     describe "#save" do
 		context "have user with id and post" do
+			let(:id) { double }
 			let(:post_valid_attribute_with_id) {{
-				"id" => "1",
+				"id" => id,
 				"text" => double
 			}}
 			let(:post_with_id) { Post.new post_valid_attribute_with_id }
@@ -20,6 +21,7 @@ describe Comment do
 			}}
 			let(:user_with_id_and_post) { User.new user_attribute_with_id_and_post }
 			let(:comment_attribute) {{
+				"id" => id,
 				"text" => double,
 				"user" => user_with_id_and_post
 			}}
@@ -28,12 +30,12 @@ describe Comment do
 			it "should call mock_query and insert_post_ref_query" do
 				mock_client = double
 				mock_query = double
-				insert_post_ref_query = "insert into postRefs (post_id, post_ref_id) values (#{@id}, #{user_with_id_and_post.post.id})"
+				insert_post_ref_query = "insert into postRefs (post_id, post_ref_id) values (#{id}, #{user_with_id_and_post.post.id})"
 
 				allow(Mysql2::Client).to receive(:new).and_return(mock_client)
 				allow(comment).to receive(:get_insert_query).and_return(mock_query)
 				expect(mock_client).to receive(:query).with(mock_query)
-				expect(mock_client).to receive(:last_id)
+				allow(mock_client).to receive(:last_id).and_return(id)
 				expect(mock_client).to receive(:query).with(insert_post_ref_query)
 
 				comment.save
