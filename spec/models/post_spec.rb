4edf83066796a714cba040530542c9b46b337e6d
Yudo Kartiko <yudoworks@gmail.com>
2021-08-18 13:07:44 +0700

    use let
    Signed-off-by: Yudo Kartiko <yudoworks@gmail.com>

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,6 +6,9 @@ describe Post do
     let(:text){ "Hello world" }
     let(:text_contain_hashtag){ "Hello world #monday" }
     let(:mock_attachment){ double }
+    let(:post_attribute) {{
+        "text" => text
+    }}
     let(:post_valid_attribute) {{
         "text" => text
     }}
@@ -34,16 +37,17 @@ describe Post do
     describe ".initialize" do
         context "given valid attribute" do
             it "should create object that equal with valid_attribute" do
-                expect(post.text).to eq(post_valid_attribute["text"])
+                post = Post.new(post_attribute)
+
+                expect(post.text).to eq(post_attribute["text"])
             end
         end
     end
 
     describe "#send_by" do
         context "given mock_user" do
-            before(:each) do
-                @mock_user = double
-            end
+            let(:mock_user) { double }
+            let(:post) { Post.new post_attribute }
 
             context "text character is not limit" do
                 before(:each) do
@@ -52,15 +56,15 @@ describe Post do
                 end
                 
                 it "does have user to equal mock_user" do
-                    post.send_by(@mock_user)
+                    post.send_by(mock_user)
 
-                    expect(post.user).to eq(@mock_user) 
+                    expect(post.user).to eq(mock_user) 
                 end 
     
                 it "does save" do
                     expect(post).to receive(:save)
     
-                    post.send_by(@mock_user)
+                    post.send_by(mock_user)
                 end 
             end
 
@@ -70,7 +74,7 @@ describe Post do
                 end
                 
                 it "does not have user" do
-                    post.send_by(@mock_user)
+                    post.send_by(mock_user)
 
                     expect(post.user).to be_nil
                 end 
@@ -78,7 +82,7 @@ describe Post do
                 it "does not save" do
                     expect(post).to_not receive(:save)
     
-                    post.send_by(@mock_user)
+                    post.send_by(mock_user)
                 end 
             end
         end
@@ -86,24 +90,20 @@ describe Post do
 
     describe "#is_characters_maximum_limit?" do
         context "have text characters length below 1000" do
-            before(:each) do
-                post_attribute = { "text" => "Hello world #monday" }
-                @post = Post.new(post_attribute)
-            end
+            let(:post_attribute) {{ "text" => "Hello world #monday" }}
+            let(:post) { Post.new post_attribute }
 
             it "does return false" do
-                actual = @post.is_characters_maximum_limit?
+                actual = post.is_characters_maximum_limit?
 
                 expect(actual).to be_falsy   
             end
         end
 
         context "post text characters length is 1000" do
-            before(:each) do
-                text = 'o' * 1000
-                post_attribute = { "text" => text }
-                @post = Post.new(post_attribute)
-            end
+            let(:text) { 'o' * 1000 }
+            let(:post_attribute) {{ "text" => text }}
+            let(:post) { Post.new post_attribute }
 
             it "does return false" do
                 actual = post.is_characters_maximum_limit?
@@ -113,14 +113,12 @@ describe Post do
         end
 
         context "post text characters length is 1001" do
-            before(:each) do
-                text = 'o' * 1001
-                post_attribute = { "text" => text }
-                @post = Post.new(post_attribute)
-            end
+            let(:text) { 'o' * 1001 }
+            let(:post_attribute) {{ "text" => text }}
+            let(:post) { Post.new post_attribute }
 
             it "does return true" do
-                actual = @post.is_characters_maximum_limit?
+                actual = post.is_characters_maximum_limit?
 
                 expect(actual).to be_truthy   
             end
@@ -128,16 +126,18 @@ describe Post do
     end
 
     describe "#save" do
+        let(:mock_query) { double }
+        let(:last_id) { double }
+        let(:post) { Post.new post_attribute }
+
         before(:each) do
-            @mock_query = double
-            @last_id = double
-            allow(post).to receive(:get_insert_query).and_return(@mock_query)
-            allow(mock_client).to receive(:last_id).and_return(@last_id)
+            allow(post).to receive(:get_insert_query).and_return(mock_query)
+            allow(mock_client).to receive(:last_id).and_return(last_id)
             allow(mock_client).to receive(:query)
         end
 
         it "does insert_query" do
-            expect(mock_client).to receive(:query).with(@mock_query)
+            expect(mock_client).to receive(:query).with(mock_query)
 
             post.save
         end
@@ -145,7 +145,7 @@ describe Post do
         it "does have id to equal last_id" do
             post.save
 
-            expect(post.id).to eq(@last_id)
+            expect(post.id).to eq(last_id)
         end
 
         context "text have hashtag" do
@@ -175,55 +175,53 @@ describe Post do
 
     describe "#get_insert_query" do
         context "have user" do
-            before(:each) do
-                @mock_user = double
-                @user_id = double
+            let(:mock_user) { double }
+            let(:user_id) { double }
 
-                allow(@mock_user).to receive(:id).and_return(@user_id)
+            before(:each) do
+                allow(mock_user).to receive(:id).and_return(user_id)
             end
 
             context "have attachment" do
-                before(:each) do
-                    @mock_saved_filename = double
-                    post_have_attachment_and_user_attribute = {
-                        "text" => text,
-                        "attachment" => mock_attachment,
-                        "user" => @mock_user
-                    }
-                    @post_have_attachment_and_user = Post.new(post_have_attachment_and_user_attribute)
+                let(:mock_saved_filename) { double }
+                let(:post_have_attachment_and_user_attribute) {{
+                    **post_attribute,
+                    "attachment" => mock_attachment,
+                    "user" => mock_user
+                }}
+                let(:post_have_attachment_and_user) { Post.new post_have_attachment_and_user_attribute }
 
+                before(:each) do
                     allow(mock_attachment).to receive(:attached_by)
-                    allow(mock_attachment).to receive(:saved_filename).and_return(@mock_saved_filename)
+                    allow(mock_attachment).to receive(:saved_filename).and_return(mock_saved_filename)
                 end
 
                 it "does save attachment with mock user" do
-                    expect(mock_attachment).to receive(:attached_by).with(@mock_user)
+                    expect(mock_attachment).to receive(:attached_by).with(mock_user)
 
-                    @post_have_attachment_and_user.get_insert_query
+                    post_have_attachment_and_user.get_insert_query
                 end
 
                 it "get insert_query_with_attachment" do
-                    expected = "insert into posts (user_id, text, attachment_path) values ('#{@user_id}','#{text}', '#{@mock_saved_filename}')"
+                    expected = "insert into posts (user_id, text, attachment_path) values ('#{user_id}','#{text}', '#{mock_saved_filename}')"
                     
-                    actual = @post_have_attachment_and_user.get_insert_query
+                    actual = post_have_attachment_and_user.get_insert_query
     
                     expect(actual).to eq(expected)  
                 end
             end
     
             context "does not have attachment" do
-                before(:each) do
-                    post_attribute_and_user_attribute = {
-                        "text" => text,
-                        "user" => @mock_user
-                    }
-                    @post_attribute_and_user = Post.new(post_attribute_and_user_attribute)
-                end
+                let(:post_attribute_and_user_attribute) {{
+                    **post_attribute,
+                    "user" => mock_user
+                }}
+                let(:post_attribute_and_user) { Post.new post_attribute_and_user_attribute }
 
                 it "does get insert_query" do
-                    expected = "insert into posts (user_id, text) values ('#{@user_id}','#{text}')"
+                    expected = "insert into posts (user_id, text) values ('#{user_id}','#{text}')"
                     
-                    actual = @post_attribute_and_user.get_insert_query
+                    actual = post_attribute_and_user.get_insert_query
     
                     expect(actual).to eq(expected)  
                 end
@@ -250,14 +248,14 @@ describe Post do
     end
     
     describe "#save_hashtags" do
-        before(:each) do 
-            hashtag_text = double
-            @hashtag_texts = [hashtag_text]
-            @hashtag = double
+        let(:hashtag_text) { double }
+        let(:hashtag_texts) { [hashtag_text] }
+        let(:hashtag) { double }
 
-            allow(Hashtag).to receive(:get_hashtags_by_text).and_return(@hashtag_texts)
-            allow(Hashtag).to receive(:new).and_return(@hashtag)
-            allow(@hashtag).to receive(:save_on)
+        before(:each) do 
+            allow(Hashtag).to receive(:get_hashtags_by_text).and_return(hashtag_texts)
+            allow(Hashtag).to receive(:new).and_return(hashtag)
+            allow(hashtag).to receive(:save_on)
         end
 
         it "does get hashtags from post text" do
@@ -265,11 +263,11 @@ describe Post do
 
             actual = post_with_id_text_contain_hashtag.save_hashtags
 
-            expect(actual).to eq(@hashtag_texts)
+            expect(actual).to eq(hashtag_texts)
         end
 
         it "should save hashtags" do
-            expect(@hashtag).to receive(:save_on).with(post_with_id)
+            expect(hashtag).to receive(:save_on).with(post_with_id)
 
             post_with_id.save_hashtags
         end
@@ -289,25 +287,25 @@ describe Post do
 
     describe ".fetch_by_id" do
         context "given id" do
+            let(:id) { "1" }
+            let(:rawData) { [post_valid_attribute_with_id] }
+
             before(:each) do
-                @id = "1"
-                rawData = [post_valid_attribute_with_id]        
-                        
                 allow(mock_client).to receive(:query).and_return(rawData)
             end
 
             it "does fetch_query with id" do
-                fetch_by_id_query = "select * from posts where id = #{@id}"
+                fetch_by_id_query = "select * from posts where id = #{id}"
 
                 expect(mock_client).to receive(:query).with(fetch_by_id_query)
                 
-                Post.fetch_by_id(@id)
+                Post.fetch_by_id(id)
             end
 
             it "does get post(id, name) to equal with post_valid_attribute_with_id" do
-                post = Post.fetch_by_id(@id)
+                post = Post.fetch_by_id(id)
 
-                expect(post.id).to eq(@id)
+                expect(post.id).to eq(id)
                 expect(post.text).to eq(post_valid_attribute_with_id["text"])
             end
         end
@@ -315,23 +313,23 @@ describe Post do
 
     describe ".fetch_by_hashtag_text" do
         context "given hashtag text" do
+            let(:hashtag_text) { "monday" }
+            let(:rawData) { [post_valid_attribute_with_id_text_contain_hashtag] }
+
             before(:each) do
-                @hashtag_text = "monday"
-                rawData = [post_valid_attribute_with_id_text_contain_hashtag]                
-    
                 allow(mock_client).to receive(:query).and_return(rawData)
             end
 
             it "does fetch by hashtag text query by hashtag text" do
-                fetch_by_hashtag_text_query = "select * from posts left join postRefs on posts.id = postRefs.post_id where postRefs.post_ref_id is null and posts.text like '%##{@hashtag_text}%';"
+                fetch_by_hashtag_text_query = "select * from posts left join postRefs on posts.id = postRefs.post_id where postRefs.post_ref_id is null and posts.text like '%##{hashtag_text}%';"
 
                 expect(mock_client).to receive(:query).with(fetch_by_hashtag_text_query)
 
-                Post.fetch_by_hashtag_text(@hashtag_text)
+                Post.fetch_by_hashtag_text(hashtag_text)
             end
 
             it "does get first posts(id and text) to equal post_with_id_text_contain_hashtag" do
-                posts = Post.fetch_by_hashtag_text(@hashtag_text)
+                posts = Post.fetch_by_hashtag_text(hashtag_text)
                 
                 expect(posts.first.id).to eq(post_with_id_text_contain_hashtag.id)
                 expect(posts.first.text).to eq(post_with_id_text_contain_hashtag.text)
