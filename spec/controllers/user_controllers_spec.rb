2432b4c8422e5c52665a76235e3fd8370ded7970
Yudo Kartiko <yudoworks@gmail.com>
2021-08-18 18:39:47 +0700

    Split user_controller.comment context given valid parameter with attachment to:
    it does fetch user by id
    it does fetch post by id
    it does create comment
    it does set attachment on post
    it does send comment on post
    
    Signed-off-by: Yudo Kartiko <yudoworks@gmail.com>

--- spec/controllers/user_controllers_spec.rb
+++ spec/controllers/user_controllers_spec.rb
@@ -121,12 +121,12 @@ describe UserController do
         let(:comment_mock) { double }
         let(:user_have_post_mock) { double }
         let(:post_id) { "1" }
+        let(:comment_attribute) {{ "text" => text }}
         let(:valid_parameter) {{
             "user_id" => user_id,
             "post_id" => post_id,
             "text" => text
         }} 
-        let(:comment_attribute) {{ "text" => text }}
 
         before(:each) do
             allow(User).to receive(:fetch_by_id).and_return(user_mock)
@@ -163,24 +163,46 @@ describe UserController do
             end
         end
 
-        context "given valid_parameter_with_attachment" do
-            it "should call user_id, post_id, comment_attribute and attachment_attribute_mock" do
-                attachment_attribute_mock = double
-                attachment_mock = double
-                valid_parameter_with_attachment = {
-                    "user_id" => user_id,
-                    "text" => text,
-                    "post_id" => post_id,
-                    "attachment" => attachment_attribute_mock
-                }
+        context "given valid parameter with attachment" do
+            let(:attachment_attribute_mock) { double } 
+            let(:attachment_mock) { double } 
+            let(:valid_parameter_with_attachment) {{
+                **valid_parameter,
+                "attachment" => attachment_attribute_mock
+            }} 
 
-                expect(User).to receive(:fetch_by_id).with(user_id).and_return(user_mock)
-                expect(Post).to receive(:fetch_by_id).with(post_id).and_return(post_mock)
-                expect(Comment).to receive(:new).with(comment_attribute).and_return(comment_mock)
-                expect(Attachment).to receive(:new).with(attachment_attribute_mock).and_return(attachment_mock)
-                allow(comment_mock).to receive(:set_attachment).with(attachment_mock)
-                allow(user_mock).to receive(:on).with(post_mock).and_return(user_mock)
-                allow(user_mock).to receive(:send).with(comment_mock)
+            before(:each) do
+                allow(Attachment).to receive(:new).and_return(attachment_mock)
+                allow(comment_mock).to receive(:set_attachment)
+            end
+
+            it "does fetch user by id" do
+                expect(User).to receive(:fetch_by_id).with(user_id)
+
+                UserController.comment(valid_parameter_with_attachment)
+            end
+
+            it "does fetch post by id" do
+                expect(Post).to receive(:fetch_by_id).with(post_id)
+
+                UserController.comment(valid_parameter_with_attachment)
+            end
+
+            it "does create comment" do
+                expect(Comment).to receive(:new).with(comment_attribute)
+
+                UserController.comment(valid_parameter_with_attachment)
+            end
+
+            it "does set attachment on post" do
+                expect(comment_mock).to receive(:set_attachment).with(attachment_mock)
+
+                UserController.comment(valid_parameter_with_attachment)
+            end
+
+            it "does send comment on post" do
+                expect(user_mock).to receive(:on).with(post_mock)
+                expect(user_have_post_mock).to receive(:send).with(comment_mock)
 
                 UserController.comment(valid_parameter_with_attachment)
             end
