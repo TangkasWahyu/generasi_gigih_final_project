29a3e25da8068f7b44cd3fcbd72aaa7c7a294a1e
Yudo Kartiko <yudoworks@gmail.com>
2021-08-18 15:14:00 +0700

    Change @variable to let
    Signed-off-by: Yudo Kartiko <yudoworks@gmail.com>

--- spec/models/attachment_spec.rb
+++ spec/models/attachment_spec.rb
@@ -23,33 +23,33 @@ describe Attachment do
 
     describe "#attached_by" do
         context "given mock user" do
+            let(:mock_file_name) { double }
+            let(:attachment) { Attachment.new attachment_attribute }
+
             before(:each) do
-                @mock_file_name = double
-                @attachment = Attachment.new(attachment_attribute)
-    
-                allow(@attachment).to receive(:get_filename).and_return(@mock_file_name)
-                allow(@attachment).to receive(:save)
+                allow(attachment).to receive(:get_filename).and_return(mock_file_name)
+                allow(attachment).to receive(:save)
             end
 
             context "is allowed" do
                 before(:each) do
-                    allow(@attachment).to receive(:is_allowed?).and_return(true)
+                    allow(attachment).to receive(:is_allowed?).and_return(true)
                 end
                 
                 it "does have user" do
                     mock_user = double
 
-                    @attachment.attached_by(mock_user)
+                    attachment.attached_by(mock_user)
 
-                    expect(@attachment.user).to eq(mock_user)
+                    expect(attachment.user).to eq(mock_user)
                 end
 
                 it "does have saved file name" do
                     mock_user = double
 
-                    @attachment.attached_by(mock_user)
+                    attachment.attached_by(mock_user)
 
-                    expect(@attachment.saved_filename).to eq(@mock_file_name)
+                    expect(attachment.saved_filename).to eq(mock_file_name)
                 end
             end
         end
@@ -57,25 +57,27 @@ describe Attachment do
 
     describe "save" do
         context "have user and saved file name" do
+            let(:mock_saved_filename) { double } 
+            let(:mock_file_read) { double } 
+            let(:attachment_attribute_with_user_and_file_name) {{
+                **attachment_attribute,
+                "saved_filename" => mock_saved_filename,
+                "user" => double
+            }} 
+            let(:attachment_have_user_and_file_name) { Attachment.new attachment_attribute_with_user_and_file_name } 
+
             before(:each) do
-                @mock_saved_filename = double
-                attachment_attribute_with_user_and_file_name = {
-                    **attachment_attribute,
-                    "saved_filename" => @mock_saved_filename,
-                    "user" => double
-                }
-                @attachment_have_user_and_file_name = Attachment.new(attachment_attribute_with_user_and_file_name)
-                allow(@attachment_have_user_and_file_name.file).to receive(:read).and_return(@mock_file_read)
+                allow(file_mock).to receive(:read).and_return(mock_file_read)
             end
 
             it "does save file in file_path" do
                 f_mock = double
-                file_path = "./public/#{@mock_saved_filename}"
+                file_path = "./public/#{mock_saved_filename}"
                 
                 expect(File).to receive(:open).with(file_path, 'w').and_yield(f_mock)
-                expect(f_mock).to receive(:write).with(@mock_file_read)
+                expect(f_mock).to receive(:write).with(mock_file_read)
     
-                @attachment_have_user_and_file_name.save
+                attachment_have_user_and_file_name.save
             end
         end
     end
@@ -85,14 +87,15 @@ describe Attachment do
             "filename" => "filename",
             "tempfile" => file_mock
         }}
+        
         context "attachment type is video/mp4" do
+            let(:attachment_attribute_with_type_video_mp4) {{
+                **attachment_attribute_without_type,
+                "type" => "video/mp4"
+            }}
+            let(:attachment_with_type_video_mp4) { Attachment.new attachment_attribute_with_type_video_mp4 }
+
             it "should be true" do
-                attachment_attribute_with_type_video_mp4 = {
-                    **attachment_attribute_without_type,
-                    "type" => "video/mp4"
-                }
-                attachment_with_type_video_mp4 = Attachment.new(attachment_attribute_with_type_video_mp4)
-                
                 expected = attachment_with_type_video_mp4.is_allowed?
 
                 expect(expected).to be_truthy 
@@ -100,13 +103,13 @@ describe Attachment do
         end
 
         context "attachment type is image/png" do
+            let(:attachment_attribute_with_type_image_png) {{
+                **attachment_attribute_without_type,
+                "type" => "image/png"
+            }}
+            let(:attachment_with_type_image_png) { Attachment.new attachment_attribute_with_type_image_png }
+
             it "should be true" do
-                attachment_attribute_with_type_image_png = {
-                    **attachment_attribute_without_type,
-                    "type" => "image/png"
-                }
-                attachment_with_type_image_png = Attachment.new(attachment_attribute_with_type_image_png)
-                
                 expected = attachment_with_type_image_png.is_allowed?
 
                 expect(expected).to be_truthy 
@@ -114,13 +117,13 @@ describe Attachment do
         end
 
         context "attachment type is image/gif" do
+            let(:attachment_attribute_with_type_image_gif) {{
+                **attachment_attribute_without_type,
+                "type" => "image/gif"
+            }}
+            let(:attachment_with_type_image_gif) { Attachment.new attachment_attribute_with_type_image_gif }
+
             it "should be true" do
-                attachment_attribute_with_type_image_gif = {
-                    **attachment_attribute_without_type,
-                    "type" => "image/gif"
-                }
-                attachment_with_type_image_gif = Attachment.new(attachment_attribute_with_type_image_gif)
-                
                 expected = attachment_with_type_image_gif.is_allowed?
 
                 expect(expected).to be_truthy 
@@ -128,13 +131,13 @@ describe Attachment do
         end
 
         context "attachment type is image/jpeg" do
+            let(:attachment_attribute_with_type_image_jpeg) {{
+                **attachment_attribute_without_type,
+                "type" => "image/jpeg"
+            }}
+            let(:attachment_with_type_image_jpeg) { Attachment.new attachment_attribute_with_type_image_jpeg }
+
             it "should be true" do
-                attachment_attribute_with_type_image_jpeg = {
-                    **attachment_attribute_without_type,
-                    "type" => "image/jpeg"
-                }
-                attachment_with_type_image_jpeg = Attachment.new(attachment_attribute_with_type_image_jpeg)
-                
                 expected = attachment_with_type_image_jpeg.is_allowed?
 
                 expect(expected).to be_truthy 
@@ -142,13 +145,13 @@ describe Attachment do
         end
 
         context "attachment type is text/plain" do
+            let(:attachment_attribute_with_type_text_plain) {{
+                **attachment_attribute_without_type,
+                "type" => "text/plain"
+            }}
+            let(:attachment_with_type_text_plain) { Attachment.new attachment_attribute_with_type_text_plain }
+
             it "should be true" do
-                attachment_attribute_with_type_text_plain = {
-                    **attachment_attribute_without_type,
-                    "type" => "text/plain"
-                }
-                attachment_with_type_text_plain = Attachment.new(attachment_attribute_with_type_text_plain)
-                
                 expected = attachment_with_type_text_plain.is_allowed?
 
                 expect(expected).to be_truthy 
@@ -156,13 +159,13 @@ describe Attachment do
         end
         
         context "attachment type is text/csv" do
+            let(:attachment_attribute_with_type_text_csv) {{
+                **attachment_attribute_without_type,
+                "type" => "text/csv"
+            }}
+            let(:attachment_with_type_text_csv) { Attachment.new attachment_attribute_with_type_text_csv }
+
             it "should be true" do
-                attachment_attribute_with_type_text_csv = {
-                    **attachment_attribute_without_type,
-                    "type" => "text/csv"
-                }
-                attachment_with_type_text_csv = Attachment.new(attachment_attribute_with_type_text_csv)
-                
                 expected = attachment_with_type_text_csv.is_allowed?
 
                 expect(expected).to be_truthy 
@@ -170,13 +173,13 @@ describe Attachment do
         end
 
         context "attachment type is application/x-tar" do
+            let(:attachment_attribute_with_type_application_x_tar) {{
+                **attachment_attribute_without_type,
+                "type" => "application/x-tar"
+            }}
+            let(:attachment_with_type_text_application_x_tar) { Attachment.new attachment_attribute_with_type_application_x_tar }
+
             it "should be false and set attachment(saved_filename) to equal NULL" do
-                attachment_attribute_with_type_application_x_tar = {
-                    **attachment_attribute_without_type,
-                    "type" => "application/x-tar"
-                }
-                attachment_with_type_text_application_x_tar = Attachment.new(attachment_attribute_with_type_application_x_tar)
-                
                 expected = attachment_with_type_text_application_x_tar.is_allowed?
 
                 expect(expected).to be_falsy
@@ -187,24 +190,23 @@ describe Attachment do
 
     describe "#get_filename" do
         context "have user" do
+            let(:attachment_attribute_have_user) {{
+                **attachment_attribute,
+                "user" => double
+            }} 
+            let(:mock_random_number) { double } 
+            let(:mock_extension) { double } 
+            let(:attachment_have_user) { Attachment.new attachment_attribute_have_user } 
+
             before(:each) do
-                attachment_attribute_have_user = {
-                    **attachment_attribute,
-                    "user" => double
-                }
-
-                @mock_random_number = double
-                @mock_extension = double
-                @attachment_have_user = Attachment.new(attachment_attribute_have_user)
-
-                allow(@attachment_have_user).to receive(:get_random_number).and_return(@mock_random_number)
-                allow(File).to receive(:extname).and_return(@mock_extension)
+                allow(attachment_have_user).to receive(:get_random_number).and_return(mock_random_number)
+                allow(File).to receive(:extname).and_return(mock_extension)
             end
 
             it "does return saved filename to equal expected" do
-                expected = "#{@mock_random_number}#{@mock_extension}"
+                expected = "#{mock_random_number}#{mock_extension}"
 
-                actual = @attachment_have_user.get_filename
+                actual = attachment_have_user.get_filename
 
                 expect(actual).to eq(expected)
             end
@@ -213,25 +215,25 @@ describe Attachment do
 
     describe "#get_random_number" do
         context "have mock_user" do
-            before(:each) do
-                mock_user = double
-                attachment_attribute_with_user = {
-                    **attachment_attribute,
-                    "user" => mock_user
-                }
-                @attachment_have_user = Attachment.new(attachment_attribute_with_user)
-                mock_user_id = double
-                mock_time_array = Array.new(10, double)
-                @joined06_mock_time_array = mock_time_array[0,6].join
+            let(:mock_user) { double } 
+            let(:mock_user_id) { double } 
+            let(:attachment_attribute_with_user) {{
+                **attachment_attribute,
+                "user" => mock_user
+            }}
+            let(:attachment_have_user) { Attachment.new attachment_attribute_with_user } 
+            let(:mock_time_array) { Array.new(10, double) } 
+            let(:joined06_mock_time_array) { mock_time_array[0,6].join } 
 
+            before(:each) do
                 allow(Time).to receive_message_chain(:new, :to_a).and_return(mock_time_array)
-                allow(mock_user).to receive_message_chain(:id).and_return(@mock_user_id)
+                allow(mock_user).to receive_message_chain(:id).and_return(mock_user_id)
             end
 
             it "does return random number" do
-                expected = "#{@joined06_mock_time_array}#{@mock_user_id}"
+                expected = "#{joined06_mock_time_array}#{mock_user_id}"
     
-                actual = @attachment_have_user.get_random_number
+                actual = attachment_have_user.get_random_number
     
                 expect(actual).to eq(expected)
             end
