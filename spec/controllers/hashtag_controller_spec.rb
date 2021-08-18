7ab8495d87aeb3fa2fc54ae81dbb90083994aead
Yudo Kartiko <yudoworks@gmail.com>
2021-08-18 22:14:12 +0700

    Refactor HashtagController.get_trending it does get trending hashtags.
    Signed-off-by: Yudo Kartiko <yudoworks@gmail.com>

--- spec/controllers/hashtag_controller_spec.rb
+++ spec/controllers/hashtag_controller_spec.rb
@@ -3,14 +3,14 @@ require_relative '../../controllers/hashtag_controller'
 
 describe HashtagController do
     describe ".get_trending" do
-        it "return top_5_hashtags_24_hours" do
-            top_5_hashtags_24_hours = ["monday", "tuesday", "wednesday", "thursday", "friday"]
-
-            allow(Hashtag).to receive(:fetch_trending).and_return(top_5_hashtags_24_hours)
+        let(:trending_hashtags) { ["monday", "tuesday", "wednesday", "thursday", "friday"] }
+        
+        it "does get trending hashtags" do
+            allow(Hashtag).to receive(:fetch_trending).and_return(trending_hashtags)
 
             actual = HashtagController.get_trending
 
-            expect(actual).to eq(top_5_hashtags_24_hours) 
+            expect(actual).to eq(trending_hashtags) 
         end
     end
 
@@ -18,7 +18,7 @@ describe HashtagController do
 		context "given valid params" do
             let(:hashtag_text){ "#monday" }
             let(:valid_params){{ "hashtag_text" => hashtag_text }}
-            
+
 			it "does fetch by hashtag text" do
 				expect(Post).to receive(:fetch_by_hashtag_text).with(hashtag_text)
 
