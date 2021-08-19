c63e64f7c1753d8210961878d76e1f20805dc612
Yudo Kartiko <yudoworks@gmail.com>
2021-08-19 14:27:51 +0700

    fix variable name is post_spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -104,15 +104,14 @@ describe Post do
     end
 
     describe "#save" do
-        let(:mock_query) { double }
         let(:last_id) { double }
         let(:mock_user) { double }
         let(:user_id) { double }
-        let(:post_attribute_and_user_attribute) {{
+        let(:post_attribute_with_user) {{
             **post_attribute,
             "user" => mock_user
         }}
-        let(:post_attribute_and_user) { Post.new post_attribute_and_user_attribute }
+        let(:post_have_user) { Post.new post_attribute_with_user }
         let(:insert_post_query) { "insert into posts (user_id, text) values ('#{user_id}','#{text}')" }
 
         before(:each) do
@@ -124,7 +123,7 @@ describe Post do
         it "does save" do
             expect(mock_client).to receive(:query).with(insert_post_query)
 
-            post_attribute_and_user.save
+            post_have_user.save
         end
 
         context "have hashtag" do
@@ -132,13 +131,11 @@ describe Post do
             let(:hashtag_texts) { [hashtag_text] }
             let(:hashtag) { double }
             let(:text_contain_hashtag) { "hello world #monday" }
-            let(:post_attribute_with_id_text_contain_hashtag) {{
-                "id" => "1",
+            let(:post_with_text_contain_hashtag_attribute) {{
                 "user" => mock_user,
-                "text" => text_contain_hashtag,
-
+                "text" => text_contain_hashtag
             }}
-            let(:post_have_id_text_contain_hashtag) { Post.new post_attribute_with_id_text_contain_hashtag }
+            let(:post_have_id_text_contain_hashtag) { Post.new post_with_text_contain_hashtag_attribute }
     
             before(:each) do
                 allow(Hashtag).to receive(:get_hashtags_by_text).and_return(hashtag_texts)
@@ -159,21 +156,20 @@ describe Post do
             end
 
             it "does not save hashtags" do
-                expect(post_attribute_and_user).to_not receive(:save_hashtags)
+                expect(post_have_user).to_not receive(:save_hashtags)
     
-                post_attribute_and_user.save
+                post_have_user.save
             end
         end
 
         context "have attachment" do
             let(:mock_attachment) { double }
             let(:mock_saved_filename) { double }
-            let(:post_have_attachment_and_user_attribute) {{
-                **post_attribute,
+            let(:post_with_attachment_and_user_attribute) {{
+                **post_attribute_with_user,
                 "attachment" => mock_attachment,
-                "user" => mock_user
             }}
-            let(:post_have_attachment_and_user) { Post.new post_have_attachment_and_user_attribute }
+            let(:post_have_attachment_and_user) { Post.new post_with_attachment_and_user_attribute }
 
             before(:each) do
                 allow(mock_attachment).to receive(:attached_by)
