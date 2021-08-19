d579b92669fac2196c4fd2ed7a16d6edd08685f8
Yudo Kartiko <yudoworks@gmail.com>
2021-08-19 21:54:10 +0700

    Remove mock user and mock post in comment_spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,32 +1,43 @@
 require_relative '../test_helper'
+require_relative '../../models/user'
+require_relative '../../models/post'
 require_relative '../../models/comment'
 
 describe Comment do
     describe "#send_by" do
-		context "given mock user" do
-			let(:id) { double } 
-			let(:mock_user) { double }
-            let(:mock_user_id) { double }
-			let(:mock_post) { double } 
-			let(:post_id) { double } 
+		context "given user have post" do
+			let(:post_text){ "Hello world?" }
+			let(:post_id) { "1" } 
+			let(:post_attribute) {{
+				"text" => post_text,
+                "id" => post_id
+			}}
+            let(:post) { Post.new post_attribute }
+
+			let(:user_id) { "1" }
+            let(:user_with_post_attribute) {{
+                "username" => "Dipsi",
+                "email" => "dipsi@teletabis.co.id",
+                "bio_description" => "am I Dipsi?",
+                "id" => user_id,
+                "post" => post
+            }}
+            let(:user_have_post) { User.new user_with_post_attribute }
+
+			let(:comment_text){ "Why Hello world?" }
+			let(:comment_attribute) {{
+				"text" => comment_text,
+			}}
+			let(:comment) { Comment.new comment_attribute }
+			
 			let(:mock_client) { double } 
 			let(:last_id) { double }
-			let(:text){ "Hello world" }
-			let(:comment_attribute) {{
-				"id" => id,
-				"text" => text,
-				"user" => mock_user
-			}}
-			let(:comment) { Comment.new comment_attribute } 
-            let(:insert_post_query) { "insert into posts (user_id, text) values ('#{mock_user_id}','#{text}')" }
-            let(:insert_post_ref_query) { "insert into postRefs (post_id, post_ref_id) values (#{id}, #{post_id})" }
+            let(:insert_post_query) { "insert into posts (user_id, text) values ('#{user_id}','#{comment_text}')" }
+            let(:insert_post_ref_query) { "insert into postRefs (post_id, post_ref_id) values (#{last_id}, #{post_id})" }
 
 			before(:each) do
 				allow(Mysql2::Client).to receive(:new).and_return(mock_client)
-                allow(mock_user).to receive(:post).and_return(mock_post)
-                allow(mock_post).to receive(:id).and_return(post_id)
                 allow(mock_client).to receive(:last_id).and_return(last_id)
-                allow(mock_user).to receive(:id).and_return(mock_user_id)
                 allow(mock_client).to receive(:query)
 			end
 		
@@ -34,7 +45,7 @@ describe Comment do
 				expect(mock_client).to receive(:query).with(insert_post_query)
 				expect(mock_client).to receive(:query).with(insert_post_ref_query)
 
-				comment.send_by(mock_user)
+				comment.send_by(user_have_post)
 			end
 		end
     end
